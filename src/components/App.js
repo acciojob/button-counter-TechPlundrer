9cc1cd4fc1d75ec8551feb18bb2c1e20e7809991
Uttam Gawas <uttam@whizti.com>
2026-03-25 11:59:18 +0530

    updated: msg

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ const App = () => {
   const handleClick = () => {
     setCount(prev => prev + 1);
   }
-  
+
   return (
     <div>
         {/* Do not remove the main div */}
-           <p>Button clicked {count} times.</p>
+           <p>Button clicked {count} times</p>
            <button onClick={handleClick}>Click Me</button>
     </div>
   )
