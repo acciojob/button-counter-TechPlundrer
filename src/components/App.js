51bbde5990e9341b0eb3cee208bccf62be5584d7
Uttam Gawas <uttam@whizti.com>
2026-03-25 11:57:14 +0530

    updated: count state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prev => prev + 1);
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
