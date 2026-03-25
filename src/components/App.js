8c5e2f9a0f470c8d967fcb2e083f0ef644cb176a
Uttam Gawas <uttam@whizti.com>
2026-03-25 11:46:45 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,17 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-           
+           <p>Button clicked {count} times.</p>
+           <button onClick={handleClick}>Click Me</button>
     </div>
   )
 }
+
+/**
+ * 
+ * git add .
+ * git commit -m "message"
+ * git push
+ */
 
 export default App
